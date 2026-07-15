316a5b3cd504311364699a3d7779518865317c97
SaranBaran <kilicsaryabaran@gmail.com>
2026-07-15 13:42:33 -0400

    checkpint adding bottom pull up

--- client/src/pages/projects.jsx
+++ client/src/pages/projects.jsx
@@ -1,7 +1,15 @@
 // Filename - pages/blogs.js
 
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const float = keyframes`
+    0% { transform: translate(0px, 0px); }
+    25% { transform: translate(6px, -10px); }
+    50% { transform: translate(-4px, -14px); }
+    75% { transform: translate(-8px, -6px); }
+    100% { transform: translate(0px, 0px); }
+`;
 
 export const ProjectTitle = styled.div`
     margin-top: 20vh;
@@ -17,6 +25,9 @@ export const ProjectsSquare = styled.div`
     height: 20vh;
     position: absolute;
     background-color: #c4c0b5;
+    box-shadow: 0 15px 25px -10px rgba(0, 0, 0, 0.2);
+    animation: ${float} ${props => props.$duration || 3}s ease-in-out infinite;
+    animation-delay: ${props => props.$delay || 0}s;
 `;
 
 export const ProjectsGallery = styled.div`
@@ -28,29 +39,29 @@ export const ProjectsGallery = styled.div`
 
 const Projects = () => {
 
-    return (
-        <body>
-        <ProjectTitle>
-            <h1>Projects</h1>
-            <p style={{width: "60vw", textAlign: "left" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        </ProjectTitle>
-        <ProjectsGallery>
-            <ProjectsSquare style={{ marginTop: "0vh", marginLeft: "80vw"}}>
-            </ProjectsSquare>
-            <ProjectsSquare style={{ marginTop: "5vh", marginLeft: "10vw"}}>
-            </ProjectsSquare>
-            <ProjectsSquare style={{ marginTop: "15vh", marginLeft: "40vw"}}>
-            </ProjectsSquare>
-            <ProjectsSquare style={{ marginTop: "50vh", marginLeft: "20vw"}}>
-            </ProjectsSquare>
-            <ProjectsSquare style={{ marginTop: "65vh", marginLeft: "50vw"}}>
-            </ProjectsSquare>
-            <ProjectsSquare style={{ marginTop: "55vh", marginLeft: "75vw"}}>
-            </ProjectsSquare>
-            <ProjectsSquare style={{ marginTop: "90vh", marginLeft: "30vw"}}>
-            </ProjectsSquare>
-        </ProjectsGallery>
-        </body>
+return (
+<div>
+    <ProjectTitle>
+        <h1>Projects</h1>
+        <p style={{width: "60vw", textAlign: "left" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </ProjectTitle>
+    <ProjectsGallery>
+        <ProjectsSquare style={{ marginTop: "0vh", marginLeft: "80vw"}} $duration={3 + Math.random() * 2} $delay={Math.random() * 2}>
+        </ProjectsSquare>
+        <ProjectsSquare style={{ marginTop: "5vh", marginLeft: "10vw"}} $duration={3 + Math.random() * 2} $delay={Math.random() * 2}>
+        </ProjectsSquare>
+        <ProjectsSquare style={{ marginTop: "15vh", marginLeft: "40vw"}} $duration={3 + Math.random() * 2} $delay={Math.random() * 2}>
+        </ProjectsSquare>
+        <ProjectsSquare style={{ marginTop: "50vh", marginLeft: "20vw"}} $duration={3 + Math.random() * 2} $delay={Math.random() * 2}>
+        </ProjectsSquare>
+        <ProjectsSquare style={{ marginTop: "65vh", marginLeft: "50vw"}} $duration={3 + Math.random() * 2} $delay={Math.random() * 2}>
+        </ProjectsSquare>
+        <ProjectsSquare style={{ marginTop: "55vh", marginLeft: "75vw"}} $duration={3 + Math.random() * 2} $delay={Math.random() * 2}>
+        </ProjectsSquare>
+        <ProjectsSquare style={{ marginTop: "90vh", marginLeft: "30vw"}} $duration={3 + Math.random() * 2} $delay={Math.random() * 2}>
+        </ProjectsSquare>
+    </ProjectsGallery>
+    </div>
     );
 };
 
