f20ada1deac1b8560c0c3071479c4a2618be7231
Saran Baran <69807636+SaranBaran@users.noreply.github.com>
2026-07-23 15:20:35 -0400

    c-c-c-cHANGEES

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,10 +11,8 @@ import {
 } from "react-router-dom";
 import Home from "./pages";
 import HomePage from "./pages/index";
-import About from "./pages/about";
 import Projects from "./pages/projects";
 import Team from "./pages/team";
-import Workshops from "./pages/workshops";
 import ScrollToTop from "./components/ScrollToTop";
 
 export const PageWrapper = styled.nav`
@@ -33,16 +31,11 @@ function App() {
                             path="/index"
                             element={<Home />}
                         />
-                        <Route path="/about" element={<About />} />
                         <Route
                             path="/projects"
                             element={<Projects />}
                         />
                         <Route path="/team" element={<Team />} />
-                        <Route
-                            path="/workshops"
-                            element={<Workshops />}
-                        />
                     </Routes>
                 </PageWrapper>
             <Footer />
