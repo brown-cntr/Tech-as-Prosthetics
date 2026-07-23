6bcf39ec1843ada42e3fa46457ce1fa8f94ef489
SaranBaran <kilicsaryabaran@gmail.com>
2026-07-22 23:12:27 -0400

    lady addded

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -12,17 +12,11 @@ const Navbar = () => {
                     <NavLink to="/index" activeStyle>
                         HomePage
                     </NavLink>
-                    <NavLink to="/about" activeStyle>
-                        About
-                    </NavLink>
                     <NavLink to="/projects" activeStyle>
                         Projects
                     </NavLink>
                     <NavLink to="/team" activeStyle>
                         Team
-                    </NavLink>
-                    <NavLink to="/workshops" activeStyle>
-                        Workshops
                     </NavLink>
                 </NavMenu>
                 <Space></Space>
--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -15,55 +15,13 @@ import { MainArea, MainTitle, MainImage, DigitalWorkshopsArea,
     HomePageTitles,
     ProjectsCircle,
     ProjectsSquare} from "./homepagecomponents";
-import cat1 from "../assets/cat1.png";
-import cat2 from "../assets/cat2.png";
-import cat3 from "../assets/cat3.png";
-import cat4 from "../assets/cat4.png";
-import cat5 from "../assets/cat5.png";
+import left-lady from "../assets/left-lady-home.svg";
 import booksVector from "../assets/books-vector.svg";
 import barsVector from "../assets/bars-vector.svg";
 import { motion, useScroll, useTransform } from "framer-motion";
 import largeCircle from "../assets/large-circle.svg";
 import ellipseShape from "../assets/ellipse.svg";
 import { useNavigate } from "react-router-dom";
-
-const digitalworkshops = [
-  {
-    to: "workshops/cat1",
-    imgSrc: cat1,
-    alt: "img1",
-    description: "Great Workshops",
-    className: "grid-one",
-  },
-  {
-    to: "workshops/cat2",
-    imgSrc: cat2,
-    alt: "img2",
-    description: "Amazing Workshops",
-    className: "grid-two",
-  },
-  {
-    to: "workshops/cat3",
-    imgSrc: cat3,
-    alt: "img3",
-    description: "SuperDuper Workshop",
-    className: "grid-three",
-  },
-  {
-    to: "workshops/cat4",
-    imgSrc: cat4,
-    alt: "img4",
-    description: "OMG Workshop",
-    className: "grid-four",
-  },
-    {
-    to: "workshops/cat4",
-    imgSrc: cat5,
-    alt: "img5",
-    description: "workiworkshop",
-    className: "grid-five",
-  },
-];
 
 
 const Home = () => {
