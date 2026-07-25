30f33890de9a18200c33dd55eaeff0273393a4f2
SaranBaran <kilicsaryabaran@gmail.com>
2026-07-25 18:48:42 -0400

    deploying website

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react()],
+  base: 'techasprosthetics.io', // Link page
 })
