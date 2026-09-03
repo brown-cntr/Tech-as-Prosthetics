f03f7e04469b54741d061967a436fd84c3d3fd0f
cc18074 <kelliemlouis@gmail.com>
2026-09-02 22:35:07 -0400

    Updates

--- client/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/',
-})
